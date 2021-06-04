c97065c6f88560a66b5056838039f28412a2e9a8
NuxiiGit <nuxii09@gmail.com>
2021-06-05 00:28:11 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -11,6 +11,28 @@ fi
 # generate the website
 ruby main.rb
 # commit changes to gh-pages branch
-
-# run the server
-python3 -m http.server 8888
+current_branch="$(git branch --show-current)"
+if [ "$current_branch" != gh-pages ] && [[ -z $(git status -s) ]]; then
+	git branch -D gh-pages
+	git branch gh-pages
+	git checkout gh-pages
+	ignore=$(head -n 2 .gitignore)
+	echo $ignore
+	echo $ignore > .gitignore
+	cat <<-EOF >> .gitignore
+		/component
+		/rouge
+		/ruby
+		.gitignore
+		Gemfile
+		*.sh
+		*.rb
+	EOF
+	git rm -r --cached .
+	git add .
+	git commit -m "published website"
+	python3 -m http.server 8888
+	git checkout "$current_branch"
+else
+	echo "make sure you are not on the gh-pages branch, and that there are no uncommitted changes"
+fi
